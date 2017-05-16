b9596c659527edb1905fe91d78553ed50c60f9ec
Shaun Scaling <shaun@shaunscaling.com>
2017-05-16 16:02:08 +0100

    More hacking, implement metadata request / response

--- kafka/kafka_protocol.go
+++ kafka/kafka_protocol.go
@@ -104,6 +104,25 @@ func (k *kafkaString) Encode(w io.Writer) {
 	}
 }
 
+func decodeString(r io.Reader) (string, error) {
+	l := new(int16)
+	readBigEndian(r, l)
+
+	if *l != int16(-1) {
+		// a non-null string
+		buff := make([]byte, *l)
+		readBigEndian(r, buff)
+		// FIXME: errors?
+		fmt.Printf("Read string '%s'\n", string(buff))
+
+		return string(buff), nil
+	} else {
+		fmt.Println("null string")
+	}
+
+	return "", nil
+}
+
 type kafkaBytes []byte
 
 func (k *kafkaBytes) Encode(w io.Writer) {
@@ -153,7 +172,16 @@ func connect() net.Conn {
 // Response
 
 type responseHeader struct {
-	size int32
+	// size is read before decoding message
+	//size          int32
+	corellationId int32
+}
+
+func (r *responseHeader) Decode(reader io.Reader) (responseHeader, error) {
+	//readBigEndian(reader, &r.size)
+	readBigEndian(reader, &r.corellationId)
+	// FIXME: errors
+	return *r, nil
 }
 
 type response interface {
@@ -174,7 +202,8 @@ type versionsResponse struct {
 
 func (r *versionsResponse) Decode(reader io.Reader) (response, error) {
 	fmt.Printf("%v\n", *r)
-	readBigEndian(reader, &r.size)
+	h, _ := r.responseHeader.Decode(reader)
+	r.responseHeader = h
 	readBigEndian(reader, &r.errorCode)
 
 	if ErrNone == r.errorCode {
@@ -187,7 +216,124 @@ func (r *versionsResponse) Decode(reader io.Reader) (response, error) {
 		return nil, errors.New(fmt.Sprintf("Received error %v\n", r.errorCode))
 	}
 
+	return r, nil
+}
+
+type metadataResponse struct {
+	responseHeader
+	brokers []metadataBrokers
+	//controllerId int32
+	metadata []topicMetadata
+}
+
+type metadataBrokers struct {
+	nodeId int32
+	host   string
+	port   int32
+	//rack   string
+}
+
+// FIXME: How to nicely fix the errors in this situation?
+func decodeMetadataBrokers(r io.Reader) (m metadataBrokers, err error) {
+	readBigEndian(r, &m.nodeId)
+	m.host, err = decodeString(r)
+	readBigEndian(r, &m.port)
+	//m.rack, err = decodeString(r)
+	return
+}
+
+type partitionMetadata struct {
+	errorCode   int16
+	partitionId int32
+	leader      int32
+	replicas    []int32
+	isr         []int32
+}
+
+func (p *partitionMetadata) Decode(reader io.Reader) (partitionMetadata, error) {
+	readBigEndian(reader, p.errorCode)
+	readBigEndian(reader, p.partitionId)
+	readBigEndian(reader, p.leader)
+
+	readArray(reader, func(reader io.Reader) {
+		i := new(int32)
+		readBigEndian(reader, i)
+		p.replicas = append(p.replicas, *i)
+	})
+
+	readArray(reader, func(reader io.Reader) {
+		i := new(int32)
+		readBigEndian(reader, i)
+		p.isr = append(p.isr, *i)
+	})
+
+	return *p, nil
+}
+
+type topicMetadata struct {
+	errorCode int16
+	topic     string
+	//isInternal bool
+	partitions []partitionMetadata
+}
+
+func readBool(r io.Reader) bool {
+	b := new(int8)
+	readBigEndian(r, b)
+	return *b == 1
+}
+
+func (r *topicMetadata) Decode(reader io.Reader) (topicMetadata, error) {
+	fmt.Printf("topicMetadata.Decode")
+
+	readBigEndian(reader, r.errorCode)
+	s, err := decodeString(reader)
+	if err == nil {
+		r.topic = s
+	}
+	//r.isInternal = readBool(reader)
+
 	fmt.Printf("%v\n", *r)
+	readArray(reader, func(reader io.Reader) {
+		var p partitionMetadata
+		result, _ := p.Decode(reader)
+		r.partitions = append(r.partitions, result)
+	})
+
+	fmt.Printf("%v\n", *r)
+	return *r, nil
+}
+
+func (r *metadataResponse) Decode(reader io.Reader) (response, error) {
+	fmt.Printf("metadataResponse.Decode %v\n", *r)
+
+	h, _ := r.responseHeader.Decode(reader)
+	r.responseHeader = h
+
+	fmt.Printf("2: %v\n", *r)
+	// [brokers] controller_id [topic_metadata]
+
+	readArray(reader, func(reader io.Reader) {
+		b, err := decodeMetadataBrokers(reader)
+		if err == nil {
+			r.brokers = append(r.brokers, b)
+		} else {
+			fmt.Println("Can't decode [brokers]")
+			// FIXME: propagate error
+		}
+	})
+
+	// version 1 only
+	// readBigEndian(reader, r.controllerId)
+	fmt.Println("Processing metadata")
+
+	readArray(reader, func(reader io.Reader) {
+		var t topicMetadata
+		metadata, _ := t.Decode(reader)
+		r.metadata = append(r.metadata, metadata)
+	})
+	fmt.Printf("%v\n", *r)
+
 	return r, nil
 }
 
@@ -196,47 +342,45 @@ func TestKafkaRead() {
 
 	conn := connect()
 	defer conn.Close()
+	/*
+		r := requestMessage{
+			requestHeader{
+				correlationId: 15,
+				apiKey:        ApiVersions,
+				apiVersion:    ApiVersionZero,
+			},
+			kafkaString(nil),
+			[]requestBody{
+				NewRequestBody(nil, VersionsRequest{}.Encode()),
+			},
+		}
+	*/
 
+	// meta-data request
 	r := requestMessage{
 		requestHeader{
-			correlationId: 15,
-			//apiKey:        ApiMetaData,
-			//apiVersion:    ApiVersionOne,
-			apiKey:     ApiVersions,
-			apiVersion: ApiVersionZero,
+			correlationId: 17,
+			apiKey:        ApiMetaData,
+			apiVersion:    ApiVersionZero,
 		},
 		kafkaString(nil),
 		[]requestBody{
-			NewRequestBody(nil, VersionsRequest{}.Encode()),
-			//NewRequestBody(nil, MetaDataRequest{"topiclogs"}.Encode()),
+			NewRequestBody(nil, MetaDataRequest{"test"}.Encode()),
 		},
 	}
 
 	err := sendMessage(conn, &r)
 
-	length := new(int32)
-	/*	err = binary.Read(conn, binary.BigEndian, length)
-		if err != nil {
-			fmt.Printf("Error reading from Kafka :%v \n", err)
-		}
-	*/
-	readBigEndian(conn, length)
-
-	fmt.Println("Response OK")
-
-	// now we need to process the response
-	// first int32 is the message size, allocate that amount of data, and read response
-	fmt.Printf("Response length : %d\n", *length)
-	response := make([]byte, *length)
-	i, err := conn.Read(response)
-	if err != nil || i != int(*length) {
-		fmt.Printf("Couldn't read response")
+	buff, err := bufferResponse(conn)
+	if err != nil {
+		fmt.Errorf("Unable to buffer response %v\n", err)
+		os.Exit(2)
 	}
-	// create a NewBuffer for io.Reader interface, no allocations used
-	fmt.Printf("Response : %v\n", response)
-	buff := bytes.NewBuffer(response)
 
-	var resp versionsResponse
+	fmt.Println("Decoding response")
+
+	//	var resp versionsResponse
+	var resp metadataResponse
 	rx, err := resp.Decode(buff)
 	if err != nil {
 		fmt.Printf("Failed to decode: %v\n", err)
@@ -245,7 +389,8 @@ func TestKafkaRead() {
 
 	fmt.Printf("%v\n", rx)
 
-	if _, err := buff.ReadByte(); err != io.EOF {
+	var notused []byte
+	if _, err := buff.Read(notused); err != io.EOF {
 		fmt.Errorf("Data still to be consumed")
 	} else {
 		fmt.Println(err)
@@ -279,7 +424,7 @@ func writeBigEndian(w io.Writer, data interface{}) {
 
 func readBigEndian(r io.Reader, data interface{}) {
 	if err := binary.Read(r, binary.BigEndian, data); err != nil {
-		fmt.Printf("Couldn't read : %v\n", data)
+		fmt.Printf("Couldn't read : %v, %v\n", data, err)
 	}
 }
 
@@ -293,4 +438,22 @@ func readArray(r io.Reader, f func(io.Reader)) {
 	}
 
 	// FIXME: Errors?
+}
+
+func bufferResponse(conn net.Conn) (io.Reader, error) {
+	length := new(int32)
+	readBigEndian(conn, length)
+
+	// now we need to process the response
+	// first int32 is the message size, allocate that amount of data, and read response
+	fmt.Printf("Response length : %d\n", *length)
+	response := make([]byte, *length)
+	i, err := conn.Read(response)
+	if err != nil || i != int(*length) {
+		return nil, errors.New("Couldn't read response")
+	}
+
+	// create a NewBuffer for io.Reader interface, no allocations used
+	fmt.Printf("Response : %v\n", response)
+	return bytes.NewBuffer(response), nil
 }
